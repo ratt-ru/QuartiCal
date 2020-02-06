b03e99dc218d43546bccb23c8f98a8db4c4315d0
JSKenyon <jskenyon007@gmail.com>
2020-02-06 10:45:59 +0200

    Fixes aliasing of unity weight arrays.

--- cubicalv2/weights/weights.py
+++ cubicalv2/weights/weights.py
@@ -1,11 +1,16 @@
 import dask.array as da
 import numpy as np
+import uuid
 
 
 def initialize_weights(xds, data_col, corr_slice, opts):
 
     if opts._unity_weights:
-        weight_col = da.ones_like(data_col[:, :1, :], dtype=np.float32)
+        n_row, n_chan, n_corr = data_col.shape
+        row_chunks, chan_chunks, corr_chunks = data_col.chunks
+        weight_col = da.ones((n_row, 1, n_corr), chunks=(row_chunks, (1,),
+                             corr_chunks), name="weights-" + uuid.uuid4().hex,
+                             dtype=np.float32)
     else:
         weight_col = xds[opts.input_ms_weight_column].data[..., corr_slice]
 
