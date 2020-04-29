9dae2ec5ea2e67e55cc5f68870956644899ba0ff
Jonathan <jonosken@gmail.com>
2020-04-29 11:52:39 +0200

    Add tests for preprocessing.

---
+++ tests/parser/test_preprocess.py
@@ -0,0 +1,62 @@
+import pytest
+from cubicalv2.parser.preprocess import interpret_model, sm_tup
+import dask.array as da
+from argparse import Namespace
+
+
+# A dictionary mapping recipe inputs to expected outputs.
+
+test_recipes = {
+    "COL1":
+        {0: ["COL1"]},
+    "~COL1":
+        {0: ['', da.subtract, 'COL1']},
+    "MODEL.lsm.html":
+        {0: [sm_tup('MODEL.lsm.html', ())]},
+    "~MODEL.lsm.html":
+        {0: ['', da.subtract, sm_tup('MODEL.lsm.html', ())]},
+    "MODEL.lsm.html@dE":
+        {0: [sm_tup('MODEL.lsm.html', ('dE',))]},
+    "MODEL.lsm.html@dE,dG":
+        {0: [sm_tup('MODEL.lsm.html', ('dE', 'dG'))]},
+    "COL1:COL2":
+        {0: ['COL1'], 1: ['COL2']},
+    "COL1:MODEL.lsm.html":
+        {0: ['COL1'], 1: [sm_tup('MODEL.lsm.html', ())]},
+    "COL1:MODEL.lsm.html@dE":
+        {0: ['COL1'], 1: [sm_tup('MODEL.lsm.html', ('dE',))]},
+    "MODEL.lsm.html:MODEL.lsm.html@dE":
+        {0: [sm_tup('MODEL.lsm.html', ())],
+         1: [sm_tup('MODEL.lsm.html', ('dE',))]},
+    "COL1~COL2":
+        {0: ['COL1', da.subtract, 'COL2']},
+    "COL1+COL2":
+        {0: ['COL1', da.add, 'COL2']},
+    "COL1~MODEL.lsm.html":
+        {0: ['COL1', da.subtract, sm_tup('MODEL.lsm.html', ())]},
+    "COL1+MODEL.lsm.html":
+        {0: ['COL1', da.add, sm_tup('MODEL.lsm.html', ())]},
+    "COL1~MODEL.lsm.html:COL2":
+        {0: ['COL1', da.subtract, sm_tup('MODEL.lsm.html', ())], 1: ['COL2']},
+    "COL1+MODEL.lsm.html:COL2":
+        {0: ['COL1', da.add, sm_tup('MODEL.lsm.html', ())], 1: ['COL2']}}
+
+
+@pytest.fixture(params=test_recipes.keys())
+def input_recipe(request):
+    return request.param
+
+
+@pytest.fixture
+def opts(input_recipe):
+
+    return Namespace(**{"input_model_recipe": input_recipe})
+
+
+@pytest.mark.preprocess
+def test_interpret_model(opts):
+
+    interpret_model(opts)
+
+    # Check that the opts has been updated with the correct internal recipe.
+    assert opts._internal_recipe == test_recipes[opts.input_model_recipe]
