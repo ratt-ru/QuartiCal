3d462904c72c3de8cf852342d57d108b9cfa5be4
Jonathan <jonosken@gmail.com>
2020-06-25 15:21:05 +0200

    Working version of 2x2 kalman filter.

--- quartical/kernels/kalman.py
+++ quartical/kernels/kalman.py
@@ -24,7 +24,7 @@ def kalman_solver(model, data, a1, a2, weights, t_map_arr, f_map_arr,
 
     n_tint, n_fint, n_ant, n_dir, n_corr = gains[active_term].shape
 
-    # invert_gains(gains, inverse_gain_list, literally(corr_mode))
+    invert_gains(gains, inverse_gain_list, literally(corr_mode))
 
     dd_term = n_dir > 1
 
@@ -38,7 +38,7 @@ def kalman_solver(model, data, a1, a2, weights, t_map_arr, f_map_arr,
         np.empty((1, n_fint, n_ant, n_dir, n_corr), dtype=last_gain.dtype)
     p_update = \
         np.empty((1, n_fint, n_ant, n_dir, n_corr), dtype=last_gain.real.dtype)
-    Q = 0.01*np.ones_like(gains[active_term], dtype=last_gain.real.dtype)
+    Q = np.ones_like(gains[active_term], dtype=last_gain.real.dtype)
     P = np.ones_like(gains[active_term], dtype=last_gain.real.dtype)
     P = P + Q
 
@@ -71,13 +71,12 @@ def kalman_solver(model, data, a1, a2, weights, t_map_arr, f_map_arr,
                         active_term,
                         literally(corr_mode))
 
-        # print(P[:, 16, 0, 0, 0])
-
         compute_update(g_update,
                        p_update,
                        jhj,
                        jhr,
-                       P[i:i+1, :, :, :, :])
+                       P[i:i+1, :, :, :, :],
+                       literally(corr_mode))
 
         gains[active_term][i:i+1, :, :, :, :] += 0.5*g_update
         P[i:i+1, :, :, :, :] -= 0.5*p_update
@@ -102,7 +101,26 @@ def kalman_solver(model, data, a1, a2, weights, t_map_arr, f_map_arr,
 
 
 @jit(nopython=True, fastmath=True, parallel=False, cache=True, nogil=True)
-def compute_update(g_update, p_update, jhj, jhr, P):
+def compute_update(g_update, p_update, jhj, jhr, P, corr_mode):
+
+    return _compute_update(g_update, p_update, jhj, jhr, P,
+                           literally(corr_mode))
+
+
+def _compute_update(g_update, p_update, jhj, jhr, P, corr_mode):
+    pass
+
+
+@overload(_compute_update, inline="always")
+def _compute_update_impl(g_update, p_update, jhj, jhr, P, corr_mode):
+
+    if corr_mode.literal_value == "diag":
+        return update_diag
+    else:
+        return update_full
+
+
+def update_diag(g_update, p_update, jhj, jhr, P, corr_mode):
 
     n_tint, n_fint, n_ant, n_dir, n_corr = jhj.shape
 
@@ -142,5 +160,62 @@ def compute_update(g_update, p_update, jhj, jhr, P):
 
                     p_update[t, f, a, d, 0] = (wb00*jhj00*P00).real
                     p_update[t, f, a, d, 1] = (wb11*jhj11*P11).real
+
+    return
+
+
+def update_full(g_update, p_update, jhj, jhr, P, corr_mode):
+
+    n_tint, n_fint, n_ant, n_dir, n_corr = jhj.shape
+
+    for t in range(n_tint):
+        for f in range(n_fint):
+            for a in range(n_ant):
+                for d in range(n_dir):
+
+                    P0 = P[t, f, a, d, 0]
+                    P1 = P[t, f, a, d, 1]
+                    P2 = P[t, f, a, d, 2]
+                    P3 = P[t, f, a, d, 3]
+
+                    Pinv0 = 1/P0
+                    Pinv1 = 1/P1
+                    Pinv2 = 1/P2
+                    Pinv3 = 1/P3
+
+                    WJHJ = np.kron(jhj[t, f, a, d, :].reshape(2, 2).T,
+                                   np.eye(2))
+
+                    PinvWJHJ = WJHJ.copy()
+
+                    PinvWJHJ[0, 0] += Pinv0
+                    PinvWJHJ[1, 1] += Pinv1
+                    PinvWJHJ[2, 2] += Pinv2
+                    PinvWJHJ[3, 3] += Pinv3
+
+                    wb = np.eye(4) - WJHJ.dot(np.linalg.inv(PinvWJHJ))
+
+                    wb[0, :] *= P0
+                    wb[1, :] *= P1
+                    wb[2, :] *= P2
+                    wb[3, :] *= P3
+
+                    g_update[t, f, a, d, :] = wb.dot(jhr[t, f, a, d, :])
+
+                    tmp = np.eye(4).astype(np.complex128)
+
+                    tmp[0, 0] = P0
+                    tmp[1, 1] = P1
+                    tmp[2, 2] = P2
+                    tmp[3, 3] = P3
+
+                    WJHJP = WJHJ.dot(tmp)
+
+                    pup = wb.dot(WJHJP)
+
+                    p_update[t, f, a, d, 0] = pup[0, 0].real
+                    p_update[t, f, a, d, 1] = pup[1, 1].real
+                    p_update[t, f, a, d, 2] = pup[2, 2].real
+                    p_update[t, f, a, d, 3] = pup[3, 3].real
 
     return
