9f29c5c8022c55ed4e14dd553d1208afbd585c82
JSKenyon <jonosken@gmail.com>
2021-01-15 10:58:54 +0200

    Split out mapping tests.

--- tests/calibration/test_calibrate.py
+++ tests/calibration/test_calibrate.py
@@ -5,30 +5,10 @@ from quartical.data_handling.ms_handler import (read_xds_list,
 from quartical.data_handling.model_handler import add_model_graph
 from quartical.calibration.calibrate import (make_gain_xds_list,
                                              add_calibration_graph)
-from quartical.calibration.mapping import (make_t_binnings,
-                                           make_t_mappings,
-                                           make_f_mappings,
-                                           make_d_mappings,
-                                           make_t_maps,
-                                           make_f_maps,
-                                           make_d_maps)
-from quartical.utils.dask import blockwise_unique
+from quartical.calibration.mapping import (make_t_maps,
+                                           make_f_maps)
 from argparse import Namespace
-import dask.array as da
 import numpy as np
-from numpy.testing import assert_array_equal
-
-
-@pytest.fixture(scope="module")
-def mapping_opts(base_opts):
-
-    # Don't overwrite base config - instead create a new Namespace and update.
-
-    options = Namespace(**vars(base_opts))
-
-    options._model_columns = ["MODEL_DATA"]
-
-    return options
 
 
 @pytest.fixture(scope="module")
@@ -81,52 +61,6 @@ def col_kwrds(_read_xds_list):
 def data_xds(data_xds_list):
 
     return data_xds_list[0]  # We only need to test on one.
-
-
-@pytest.fixture(scope="module")
-def t_map_arr(data_xds, xds_opts):
-
-    time_col = data_xds.TIME.data
-    interval_col = data_xds.INTERVAL.data
-
-    # Convert the time column data into indices. Chunks is expected to be a
-    # tuple of tuples.
-    utime_chunks = data_xds.UTIME_CHUNKS
-    _, utime_loc, utime_ind = blockwise_unique(time_col,
-                                               (utime_chunks,),
-                                               return_index=True,
-                                               return_inverse=True)
-
-    # Assosciate each unique time with an interval. This assumes that all
-    # rows at a given time have the same interval as the alternative is
-    # madness.
-    utime_intervals = da.map_blocks(
-        lambda arr, inds: arr[inds],
-        interval_col,
-        utime_loc,
-        chunks=utime_loc.chunks,
-        dtype=np.float64)
-
-    # Daskify the chunks per array - these are already known from the
-    # initial chunking step.
-    utime_per_chunk = da.from_array(utime_chunks,
-                                    chunks=(1,))
-
-    t_bin_arr = make_t_binnings(utime_per_chunk, utime_intervals, xds_opts)
-    t_map_arr = make_t_mappings(utime_ind, t_bin_arr)
-
-    return t_map_arr
-
-
-@pytest.fixture(scope="module")
-def f_map_arr(data_xds, xds_opts):
-
-    chan_freqs = data_xds.CHAN_FREQ.data
-    chan_widths = data_xds.CHAN_WIDTH.data
-
-    f_map_arr = make_f_mappings(chan_freqs, chan_widths, xds_opts)
-
-    return f_map_arr
 
 
 @pytest.fixture(scope="module")
@@ -211,11 +145,6 @@ def _add_calibration_graph(data_xds_list, col_kwrds, xds_opts):
 
 
 @pytest.fixture(scope="module")
-def t_bin_list(data_xds_list, xds_opts):
-    return make_t_maps(data_xds_list, xds_opts)[0]
-
-
-@pytest.fixture(scope="module")
 def t_map_list(data_xds_list, xds_opts):
     return make_t_maps(data_xds_list, xds_opts)[1]
 
@@ -223,11 +152,6 @@ def t_map_list(data_xds_list, xds_opts):
 @pytest.fixture(scope="module")
 def f_map_list(data_xds_list, xds_opts):
     return make_f_maps(data_xds_list, xds_opts)
-
-
-@pytest.fixture(scope="module")
-def d_map_list(data_xds_list, xds_opts):
-    return make_d_maps(data_xds_list, xds_opts)
 
 
 @pytest.fixture(scope="module")
@@ -250,166 +174,6 @@ def solved_gain_xds_list(_add_calibration_graph):
 def post_cal_data_xds_list(_add_calibration_graph):
 
     return _add_calibration_graph[1]
-
-# ------------------------------make_t_binnings--------------------------------
-
-
-@pytest.mark.parametrize("time_chunk", [33, 60])
-def test_t_binnings(time_int, time_chunk, mapping_opts):
-    """Test construction of time mappings for different chunks/intervals."""
-
-    opts = mapping_opts
-    opts.G_time_interval = time_int  # Setting time interval on first gain.
-    opts.B_time_interval = time_int*2  # Setting time interval on second gain.
-
-    n_time = 100  # Total number of unique times to consider.
-    n_bl = 351  # 27 antenna array - VLA-like.
-    interval = 8
-
-    time_stamps = da.arange(100, chunks=time_chunk)
-
-    utime_per_chunk = da.from_array(time_stamps.chunks[0],
-                                    chunks=(1,))
-
-    time_col = da.map_blocks(
-        np.repeat,
-        time_stamps,
-        n_bl,
-        chunks=(tuple(c*n_bl for c in time_stamps.chunks[0]),))
-
-    interval_col = da.ones_like(time_col)*interval
-
-    utime_loc = da.map_blocks(
-        lambda a, **kwargs: np.unique(a, **kwargs)[1],
-        time_col,
-        return_index=True,
-        chunks=time_stamps.chunks)
-
-    utime_intervals = da.map_blocks(
-        lambda arr, inds: arr[inds],
-        interval_col,
-        utime_loc,
-        chunks=utime_loc.chunks,
-        dtype=np.float64)
-
-    da_t_bins = make_t_binnings(utime_per_chunk, utime_intervals, opts)
-
-    t_ints = [getattr(opts, t + "_time_interval") or n_time
-              for t in opts.solver_gain_terms]
-
-    for block_ind in range(da_t_bins.npartitions):
-        binning = da_t_bins.blocks[block_ind].compute()
-        ivl_col = interval_col.blocks[block_ind].compute()
-        for g_ind, t_int in enumerate(t_ints):
-            if isinstance(t_int, float):
-                sol_widths = np.zeros(np.max(binning[:, g_ind] + 1))
-                for ivl_ind, target in enumerate(binning[:, g_ind]):
-                    sol_widths[target] += ivl_col[ivl_ind]
-                assert all(sol_widths[:-1] >= t_int)
-            else:
-                assert all(np.unique(binning[:, g_ind],
-                                     return_counts=True)[1] <= t_int)
-
-# ------------------------------make_t_mappings--------------------------------
-
-# TODO: This test has been removed temporarily because the binner does the
-# majority of this work now. All the mapping code does is select out to the
-# appropriate resolution.
-
-# @pytest.mark.parametrize("time_chunk", [33, 60])
-# def test_t_mappings(time_int, time_chunk, mapping_opts):
-#     """Test construction of time mappings for different chunks/intervals."""
-
-#     opts = mapping_opts
-#     opts.G_time_interval = time_int  # Setting time interval on first gain.
-#     opts.B_time_interval = time_int*2  # Setting time interval on second
-#                                          gain.
-
-#     n_time = 100  # Total number of unique times to consider.
-#     n_bl = 351  # 27 antenna array - VLA-like.
-
-#     time_stamps = da.arange(100, chunks=time_chunk)
-
-#     time_col = da.map_blocks(
-#         np.repeat,
-#         time_stamps,
-#         n_bl,
-#         chunks=(tuple(c*n_bl for c in time_stamps.chunks[0]),))
-
-#     utime_ind = da.map_blocks(
-#         lambda a, **kwargs: np.unique(a, **kwargs)[1],
-#         time_col,
-#         return_inverse=True)
-
-#     da_t_maps = make_t_mappings(utime_ind, opts)
-
-#     # Set up and compute numpy values to test against.
-
-#     t_ints = [getattr(opts, t + "_time_interval") or n_time
-#               for t in opts.solver_gain_terms]
-
-#     np_t_maps = np.array(list(map(lambda ti: utime_ind//ti, t_ints))).T
-
-#     assert_array_equal(da_t_maps, np_t_maps)
-
-
-# ------------------------------make_f_mappings--------------------------------
-
-
-@pytest.mark.parametrize("freq_chunk", [30, 64])
-def test_f_mappings(freq_int, freq_chunk, mapping_opts):
-    """Test construction of freq mappings for different chunks/intervals."""
-
-    opts = mapping_opts
-    opts.G_freq_interval = freq_int  # Setting freq interval on first gain.
-    opts.B_freq_interval = freq_int*2  # Setting freq interval on second gain.
-
-    n_freq = 64  # Total number of channels to consider.
-
-    chan_freqs = da.arange(n_freq, chunks=freq_chunk)
-    chan_widths = da.ones(n_freq, chunks=freq_chunk)*7
-
-    da_f_maps = make_f_mappings(chan_freqs, chan_widths, opts)
-
-    # Set up and compute numpy values to test against.
-
-    f_ints = [getattr(opts, t + "_freq_interval") or n_freq
-              for t in opts.solver_gain_terms]
-
-    for block_ind in range(da_f_maps.npartitions):
-        f_map = da_f_maps.blocks[block_ind].compute()
-        chan_width = chan_widths.blocks[block_ind].compute()
-        for g_ind, f_int in enumerate(f_ints):
-            if isinstance(f_int, float):
-                sol_widths = np.zeros(np.max(f_map[:, g_ind] + 1))
-                for ivl_ind, target in enumerate(f_map[:, g_ind]):
-                    sol_widths[target] += chan_width[ivl_ind]
-                assert all(sol_widths[:-1] >= f_int)
-            else:
-                assert all(np.unique(f_map[:, g_ind],
-                                     return_counts=True)[1] <= f_int)
-
-# ------------------------------make_d_mappings--------------------------------
-
-
-@pytest.mark.parametrize("n_dir", [2, 10])
-@pytest.mark.parametrize("has_dd_term", [False, True])
-def test_d_mappings(n_dir, has_dd_term, mapping_opts):
-    """Test construction of direction mappings for different n_dir."""
-
-    opts = mapping_opts
-    opts.B_direction_dependent = has_dd_term
-
-    d_maps = make_d_mappings(n_dir, opts)  # Not a dask array.
-
-    # Set up and compute numpy values to test against.
-
-    dd_terms = [getattr(opts, t + "_direction_dependent")
-                for t in opts.solver_gain_terms]
-
-    np_d_maps = np.array(list(map(lambda dd: np.arange(n_dir)*dd, dd_terms)))
-
-    assert_array_equal(d_maps, np_d_maps)
 
 
 # ----------------------------make_gain_xds_list-------------------------------
@@ -517,7 +281,7 @@ def test_has_gain_field(solved_gain_xds_list):
 
 
 # def test_write_columns(post_cal_data_xds_list, xds_opts):
-#     """Check that the output column name is added to WRTIE_COLS."""
+#     """Check that the output column name is added to WRITE_COLS."""
 
 #     assert all([col in xds.attrs["WRITE_COLS"]
 #                 for col in xds_opts.output_column
