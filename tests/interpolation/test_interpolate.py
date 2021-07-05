9ac7bd98e13bb84c28e1ce507e0997b888035e52
JSKenyon <jonathan.simon.kenyon@gmail.com>
2021-07-05 11:29:56 +0200

    Refine dataset creation.

--- tests/interpolation/test_interpolate.py
+++ tests/interpolation/test_interpolate.py
@@ -9,47 +9,32 @@ import numpy as np
 from copy import deepcopy
 
 
-@pytest.fixture(scope="module")
-def opts(base_opts):
-
-    # Don't overwrite base config - instead duplicate and update.
-
-    _opts = deepcopy(base_opts)
-
-    _opts.solver.terms = ["G"]
-    _opts.G.load_from = ""
-    _opts.G.interp_method = "2dlinear"
-    _opts.G.interp_mode = "reim"
-    _opts.B.load_from = ""
-    _opts.B.interp_method = "2dlinear"
-    _opts.B.interp_mode = "ampphase"
-
-    return _opts
-
-
-@pytest.fixture(scope="module")
-def chain_opts(opts):
-    return gains_to_chain(opts)
-
-
-def mock_gain_xds_list(n_time, n_freq, t_offset, f_offset, n_xds_t, n_xds_f):
+def mock_gain_xds_list(start_time,
+                       n_time,
+                       gap_time,
+                       n_xds_time,
+                       start_freq,
+                       n_freq,
+                       gap_freq,
+                       n_xds_freq):
 
     n_ant = 7
     n_dir = 1
     n_corr = 4
 
-    along_time = list(range(n_xds_t))
-    along_freq = list(range(n_xds_f))
-
-    time_ranges = [np.arange(n_time) + (n + 1)*t_offset for n in along_time]
-    freq_ranges = [np.arange(n_freq) + (n + 1)*f_offset for n in along_freq]
-
     _gain_xds_list = []
 
-    for t_ind, f_ind in product(along_time, along_freq):
+    for t_ind, f_ind in product(range(n_xds_time), range(n_xds_freq)):
+
+        time_lb = start_time + t_ind*(n_time + gap_time)
+        time_range = np.arange(time_lb, time_lb + n_time)
+
+        freq_lb = start_freq + f_ind*(n_freq + gap_freq)
+        freq_range = np.arange(freq_lb, freq_lb + n_freq)
+
         coords = {
-            "gain_t": time_ranges[t_ind],
-            "gain_f": freq_ranges[f_ind],
+            "gain_t": time_range,
+            "gain_f": freq_range,
             "ant": np.arange(n_ant),
             "dir": np.arange(n_dir),
             "corr": np.arange(n_corr)
@@ -81,16 +66,41 @@ def mock_gain_xds_list(n_time, n_freq, t_offset, f_offset, n_xds_t, n_xds_f):
 
 
 @pytest.fixture(scope="module")
+def opts(base_opts):
+
+    # Don't overwrite base config - instead duplicate and update.
+
+    _opts = deepcopy(base_opts)
+
+    _opts.solver.terms = ["G"]
+    _opts.G.load_from = ""
+    _opts.G.interp_method = "2dlinear"
+    _opts.G.interp_mode = "reim"
+    _opts.B.load_from = ""
+    _opts.B.interp_method = "2dlinear"
+    _opts.B.interp_mode = "ampphase"
+
+    return _opts
+
+
+@pytest.fixture(scope="module")
+def chain_opts(opts):
+    return gains_to_chain(opts)
+
+
+@pytest.fixture(scope="module")
 def gain_xds_list():
-    return [[xds] for xds in mock_gain_xds_list(10, 2, 10, 4, 3, 3)]
+    return [[xds] for xds in mock_gain_xds_list(10, 10, 10, 3, 4, 4, 2, 3)]
 
 
 def test_load_and_interpolate_gains(gain_xds_list,
                                     chain_opts,
                                     monkeypatch):
 
-    monkeypatch.setattr("quartical.interpolation.interpolate.xds_from_zarr",
-                        lambda store: mock_gain_xds_list(10, 2, 10, 4, 3, 3))
+    monkeypatch.setattr(
+        "quartical.interpolation.interpolate.xds_from_zarr",
+        lambda store: mock_gain_xds_list(10, 10, 10, 3, 4, 4, 2, 3)
+    )
 
     interp_xds_list = load_and_interpolate_gains(gain_xds_list, chain_opts)
 
