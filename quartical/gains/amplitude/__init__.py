907f599219b13ac966b65225e373b5d656622c6a
JSKenyon <jonathan.simon.kenyon@gmail.com>
2021-08-27 09:17:33 +0200

    Initial commit of beginnings of amplitude solver.

---
+++ quartical/gains/amplitude/__init__.py
@@ -0,0 +1,42 @@
+from quartical.gains.gain import Gain, gain_spec_tup, param_spec_tup
+from quartical.gains.amplitude.kernel import amplitude_solver, amplitude_args
+import numpy as np
+
+
+class Amplitude(Gain):
+
+    solver = amplitude_solver
+    term_args = amplitude_args
+
+    def __init__(self, term_name, term_opts, data_xds, coords, tipc, fipc):
+
+        Gain.__init__(self, term_name, term_opts, data_xds, coords, tipc, fipc)
+
+        self.n_ppa = 1
+        self.gain_chunk_spec = gain_spec_tup(self.n_tipc_g,
+                                             self.n_fipc_g,
+                                             (self.n_ant,),
+                                             (self.n_dir,),
+                                             (self.n_corr,))
+        self.param_chunk_spec = param_spec_tup(self.n_tipc_g,
+                                               self.n_fipc_g,
+                                               (self.n_ant,),
+                                               (self.n_dir,),
+                                               (self.n_ppa,),
+                                               (self.n_corr,))
+        self.gain_axes = ("gain_t", "gain_f", "ant", "dir", "corr")
+        self.param_axes = ("param_t", "param_f", "ant", "dir", "param", "corr")
+
+    def make_xds(self):
+
+        xds = Gain.make_xds(self)
+
+        xds = xds.assign_coords({"param": np.array(["amplitude"]),
+                                 "param_t": self.param_times,
+                                 "param_f": self.param_freqs})
+        xds = xds.assign_attrs({"GAIN_SPEC": self.gain_chunk_spec,
+                                "PARAM_SPEC": self.param_chunk_spec,
+                                "GAIN_AXES": self.gain_axes,
+                                "PARAM_AXES": self.param_axes})
+
+        return xds
