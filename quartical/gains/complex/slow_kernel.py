4bb1fed5893c0e57da665266aed7e3678c2dad5d
JSKenyon <jonathan.simon.kenyon@gmail.com>
2021-09-16 11:12:30 +0200

    Propagate changes to slow_complex solver.

--- quartical/gains/complex/slow_kernel.py
+++ quartical/gains/complex/slow_kernel.py
@@ -42,11 +42,12 @@ def slow_complex_solver(base_args, term_args, meta_args, corr_mode):
         a1 = base_args.a1
         a2 = base_args.a2
         weights = base_args.weights
+        flags = base_args.flags
         t_map_arr = base_args.t_map_arr[0]  # Ignore parameter mappings.
         f_map_arr = base_args.f_map_arr[0]  # Ignore parameter mappings.
         d_map_arr = base_args.d_map_arr
         gains = base_args.gains
-        flags = base_args.flags
+        gain_flags = base_args.flags
         row_map = base_args.row_map
         row_weights = base_args.row_weights
 
@@ -93,6 +94,7 @@ def slow_complex_solver(base_args, term_args, meta_args, corr_mode):
                             a1,
                             a2,
                             weights,
+                            flags,
                             t_map_arr,
                             f_map_arr,
                             d_map_arr,
@@ -138,7 +140,7 @@ def slow_complex_solver(base_args, term_args, meta_args, corr_mode):
                parallel=True,
                cache=True,
                nogil=True)
-def compute_jhj_jhr(jhj, jhr, model, gains, residual, a1, a2, weights,
+def compute_jhj_jhr(jhj, jhr, model, gains, residual, a1, a2, weights, flags,
                     t_map_arr, f_map_arr, d_map_arr, row_map, row_weights,
                     active_term, corr_mode):
 
@@ -155,7 +157,7 @@ def compute_jhj_jhr(jhj, jhr, model, gains, residual, a1, a2, weights,
     set_identity = factories.set_identity_factory(corr_mode)
     compute_jhwj_jhwr_elem = compute_jhwj_jhwr_elem_factory(corr_mode)
 
-    def impl(jhj, jhr, model, gains, residual, a1, a2, weights,
+    def impl(jhj, jhr, model, gains, residual, a1, a2, weights, flags,
              t_map_arr, f_map_arr, d_map_arr, row_map, row_weights,
              active_term, corr_mode):
         _, n_chan, n_dir, n_corr = model.shape
@@ -221,6 +223,9 @@ def compute_jhj_jhr(jhj, jhr, model, gains, residual, a1, a2, weights,
                 a1_m, a2_m = a1[row], a2[row]
 
                 for f in range(fs, fe):
+
+                    if flags[row, f]:  # Skip flagged data points.
+                        continue
 
                     r = residual[row, f]
                     w = weights[row, f]  # Consider a map?
