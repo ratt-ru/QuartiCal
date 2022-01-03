24f959f6a7b3be17071498c16b48353e74e128f6
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-01-03 14:04:05 +0200

    [skip ci] Add tiny reminder of other utils to main help.

--- quartical/config/helper.py
+++ quartical/config/helper.py
@@ -21,7 +21,8 @@ GAIN_MSG = "Gains make use of a special configuration " \
 HELP_MSG = f"For full help, use 'goquartical help'. For help with a " \
            f"specific section, use e.g. 'goquartical help='[section1," \
            f"section2]''. Help is available for " \
-           f"[{', '.join(HELPSTRINGS.keys())}]."
+           f"[{', '.join(HELPSTRINGS.keys())}]. Other command line " \
+           f"utilitites: [goquartical-backup, goquartical-restore]."
 
 
 def populate(typ, help_str, help_dict=None):
