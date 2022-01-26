33026aab59a5b8a323740daf7c1814c8e007ad08
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-01-26 14:34:30 +0200

    Further progress on summary applet.

--- quartical/apps/stats.py
+++ quartical/apps/stats.py
@@ -5,8 +5,9 @@ import dask.array as da
 import numpy as np
 from loguru import logger
 import logging
-from quartical.logging import InterceptHandler
 import sys
+from quartical.logging import InterceptHandler
+from quartical.data_handling import CORR_TYPES
 
 
 def configure_loguru(output_dir):
@@ -61,14 +62,14 @@ def antenna_info(path):
 
 def data_desc_info(path):
 
-    dd_xds_list = xds_from_table(
+    dd_xds_list = xds_from_table(  # noqa
         path + "::DATA_DESCRIPTION",
         group_cols=["__row__"],
         chunks={"row": 1, "chan": -1}
     )
 
-    # Not printing any summary information for this subtable yet - it is more
-    # an implementation detail than useful information.
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
 
 
 def feed_info(path):
@@ -87,9 +88,9 @@ def feed_info(path):
 
     msg = "Feed summary:\n"
     msg += "    {:<4} {:<8} {:<8} {:<16}\n".format("SPW",
-                                                    "ANTENNA",
-                                                    "POL_TYPE",
-                                                    "RECEPTOR_ANGLE")
+                                                   "ANTENNA",
+                                                   "POL_TYPE",
+                                                   "RECEPTOR_ANGLE")
 
     zipper = zip(ant_id_per_xds, pol_type_per_xds, rec_angle_per_xds)
 
@@ -99,6 +100,83 @@ def feed_info(path):
                    f"{'{} {}'.format(*vals[2]):<16}\n"
 
     logger.info(msg)
+
+
+def flag_cmd_info(path):
+
+    flag_cmd_xds = xds_from_table(path + "::FLAG_CMD")  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
+
+
+def field_info(path):
+
+    field_xds = xds_from_table(path + "::FIELD")[0]
+
+    ids = [i for i in field_xds.SOURCE_ID.values]
+    names = [n for n in field_xds.NAME.values]
+    phase_dirs = [pd for pd in field_xds.PHASE_DIR.values]
+    ref_dirs = [rd for rd in field_xds.REFERENCE_DIR.values]
+    delay_dirs = [dd for dd in field_xds.REFERENCE_DIR.values]
+
+    msg = "Field summary:\n"
+    msg += "    {:<4} {:<16} {:<16} {:<16} {:<16}\n".format("ID", "NAME",
+                                                            "PHASE_DIR",
+                                                            "REF_DIR",
+                                                            "DELAY_DIR")
+
+    zipper = zip(ids, names, phase_dirs, ref_dirs, delay_dirs)
+
+    for vals in zipper:
+        msg += f"    {vals[0]:<4} {vals[1]:<16} " \
+               f"{'{:.4f} {:.4f}'.format(*vals[2][0]):<16} " \
+               f"{'{:.4f} {:.4f}'.format(*vals[3][0]):<16} " \
+               f"{'{:.4f} {:.4f}'.format(*vals[4][0]):<16}"
+
+    logger.info(msg)
+
+
+def history_info(path):
+
+    history_xds = xds_from_table(path + "::HISTORY")[0]  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
+
+
+def observation_info(path):
+
+    observation_xds = xds_from_table(path + "::OBSERVATION")[0]  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
+
+
+def polarization_info(path):
+
+    polarization_xds = xds_from_table(path + "::POLARIZATION")[0]
+
+    corr_types = polarization_xds.CORR_TYPE.values
+
+    readable_corr_types = \
+        [[CORR_TYPES.get(ct, '-') for ct in cta] for cta in corr_types]
+
+    msg = "Polarization summary:\n"
+    msg += "    {:<8} {:<30}\n".format("INDEX", "CORR_TYPE")
+
+    for i, vals in enumerate(zip(corr_types, readable_corr_types)):
+        msg += f"    {i:<8} {'{:} -> {:}'.format(vals[0], vals[1]):<30}"
+
+    logger.info(msg)
+
+
+def processor_info(path):
+
+    processor_xds = xds_from_table(path + "::PROCESSOR")[0]  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
 
 
 def spw_info(path):
@@ -121,6 +199,71 @@ def spw_info(path):
 
     for i, vals in enumerate(zip(n_chan_per_spw, bw_per_spw, ref_per_spw)):
         msg += f"    {i:<8} {vals[0]:<10} {vals[1]:<16} {vals[2]:<16}\n"
+
+    logger.info(msg)
+
+
+def state_info(path):
+
+    state_xds = xds_from_table(path + "::STATE")[0]  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
+
+
+def source_info(path):
+
+    source_xds = xds_from_table(path + "::SOURCE")[0]  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
+
+
+def pointing_info(path):
+
+    pointing_xds = xds_from_table(path + "::POINTING")[0]  # noqa
+
+    # Not printing any summary information for this subtable yet - not sure
+    # what is relevant.
+
+
+def flagging_summary(xds_list):
+
+    n_flag_per_xds = []
+    n_elem_per_xds = []
+    perc_flagged_per_xds = []
+
+    for xds in xds_list:
+
+        flags = xds.FLAG.data | xds.FLAG_ROW.data[:, None, None]
+
+        n_flag = da.sum(flags)
+        n_elem = flags.size
+        flag_perc = (n_flag/n_elem)*100
+
+        n_flag_per_xds.append(n_flag)
+        n_elem_per_xds.append(n_elem)
+        perc_flagged_per_xds.append(flag_perc)
+
+    total_n_flag = da.sum(da.stack(n_flag_per_xds))
+    total_n_elem = da.sum(da.stack(n_elem_per_xds))
+    total_flag_perc = (total_n_flag/total_n_elem)*100
+
+    total_flag_perc, perc_flagged_per_xds = \
+        da.compute(total_flag_perc, perc_flagged_per_xds)
+
+    fields = ("DATA_DESC_ID", "SCAN_NUMBER", "FIELD_ID", "PERC_FLAGGED")
+
+    msg = "Flagging summary:\n"
+    msg += "    {:<12} {:<12} {:<12} {:<12}\n".format(*fields)
+
+    for idx, xds in enumerate(xds_list):
+        msg += f"    {xds.DATA_DESC_ID:<12} {xds.SCAN_NUMBER:<12} " \
+               f"{xds.FIELD_ID:<12} {perc_flagged_per_xds[idx]:<12.2f}\n"
+
+    fields = ("", "", "TOTAL", total_flag_perc)
+
+    msg += "    {:<12} {:<12} {:<12} {:<12.2f}\n".format(*fields)
 
     logger.info(msg)
 
@@ -148,52 +291,30 @@ def summary():
 
     configure_loguru(output_dir)
 
+    # Get summary info for subtables. TODO: Improve as needed.
     antenna_info(path)
     data_desc_info(path)
     feed_info(path)
+    flag_cmd_info(path)
+    field_info(path)
+    history_info(path)
+    observation_info(path)
+    polarization_info(path)
+    processor_info(path)
     spw_info(path)
+    state_info(path)
+    source_info(path)
+    pointing_info(path)
 
-    # # Determine the number/type of correlations present in the measurement set.
-    # pol_xds = xds_from_table(ms_opts.path + "::POLARIZATION")[0]
+    # Open the data, grouping by the usual columns. Use these datasets to
+    # produce some useful summaries.
 
-    # try:
-    #     corr_types = [CORR_TYPES[ct] for ct in pol_xds.CORR_TYPE.values[0]]
-    # except KeyError:
-    #     raise KeyError("Data contains unsupported correlation products.")
+    data_xds_list = xds_from_ms(
+        path,
+        index_cols=("TIME",),
+        columns=("FLAG", "FLAG_ROW"),
+        group_cols=("DATA_DESC_ID", "SCAN_NUMBER", "FIELD_ID"),
+        chunks={"row": 25000, "chan": 1024, "corr": -1},
+    )
 
-    # n_corr = len(corr_types)
-
-    # if n_corr not in (1, 2, 4):
-    #     raise ValueError(f"Measurement set contains {n_corr} correlations - "
-    #                      f"this is not supported.")
-
-    # logger.info(f"Polarization table indicates {n_corr} correlations are "
-    #             f"present in the measurement set - {corr_types}.")
-
-    # # Determine the phase direction from the measurement set. TODO: This will
-    # # probably need to be done on a per xds basis. Can probably be accomplished
-    # # by merging the field xds grouped by DDID into data grouped by DDID.
-
-    # field_xds = xds_from_table(ms_opts.path + "::FIELD")[0]
-    # phase_dir = np.squeeze(field_xds.PHASE_DIR.values)
-    # field_names = field_xds.NAME.values
-
-    # logger.info("Field table indicates phase centre is at ({} {}).",
-    #             phase_dir[0], phase_dir[1])
-
-
-    # data_xds_list = xds_from_ms(
-    #     path,
-    #     index_cols=("TIME",),
-    #     columns=("TIME"),
-    #     group_cols=("DATA_DESC_ID", "SCAN_NUMBER", "FIELD_ID"),
-    #     chunks={"row": "auto", "chan": "auto", "corr": -1},
-    #     table_schema=["MS", {**schema}])
-
-    # restored_xds_list = xds_to_table(
-    #     zarr_xds_list,
-    #     str(ms_path),
-    #     columns=(args.column,)
-    # )
-
-    # dask.compute(restored_xds_list)
+    flagging_summary(data_xds_list)
