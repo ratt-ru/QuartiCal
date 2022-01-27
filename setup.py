a1b28ddb848cd680def83197147512fc683d3993
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-01-27 10:51:11 +0200

    Rename file.

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
             'goquartical-config = quartical.config.parser:create_user_config',
             'goquartical-backup = quartical.apps.backup:backup',
             'goquartical-restore = quartical.apps.backup:restore',
-            'goquartical-summary = quartical.apps.stats:summary'
+            'goquartical-summary = quartical.apps.summary:summary'
         ]
     },
 )
