bfbc77d65c0140ed0c3025d68758577fd3c12bd3
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-01-27 11:34:10 +0200

    Fix doc breakage caused by new schema.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,10 +2,13 @@ from omegaconf import OmegaConf as oc
 from pathlib import Path
 
 path = Path(__file__).resolve()
-path_to_helpstrings = \
-    path.parents[2].joinpath("quartical/config/helpstrings.yaml")
-HELPSTRINGS = oc.load(path_to_helpstrings)
+config_path = path.parents[2].joinpath("quartical/config")
+HELPSTRINGS = oc.load(config_path.joinpath("argument_schema.yaml"))
+HELPSTRINGS.update(oc.load(config_path.joinpath("gain_schema.yaml")))
 
+# We want to pull out just the helpstrings and not all the scabha info.
+HELPSTRINGS = {k: {kk: vv["info"] for kk, vv in v.items()}
+               for k, v in HELPSTRINGS.items()}
 
 # Configuration file for the Sphinx documentation builder.
 #
