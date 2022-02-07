afbf26789a1d87d6862a2cb9b0103f522385bff5
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-02-07 09:09:55 +0200

    Finish changing license.

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# QuartiCal: a radio interferometric calibration suite
-# (c) 2019 Rhodes University & Jonathan S. Kenyon
-# https://github.com/JSKenyon/QuartiCal
-# This code is distributed under the terms of GPLv2, see LICENSE.md for
-# details.
-#
-# Copyright (c) 2019 SKA South Africa
-#
-# This file is part of QuartiCal.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import quartical
 from setuptools import setup, find_packages
@@ -57,7 +34,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7"
@@ -67,7 +44,7 @@ setup(
     ],
     author='Jonathan Kenyon',
     author_email='jonosken@gmail.com',
-    license='GNU GPL v3',
+    license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
