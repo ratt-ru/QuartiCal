Fix converged perc going over 100. Change normalisation approach.

quartical/calibration/solver.py
@@ -156,7 +156,7 @@ def solver_wrapper(term_spec_list, solver_opts, chain_opts, **kwargs):
             jhj = jhj[:, :, :, :, range(jhj.shape[-2]), range(jhj.shape[-1])]
 
         results_dict[f"{term_name}-conviter"] += np.atleast_2d(info_tup[0])
-        results_dict[f"{term_name}-convperc"] += np.atleast_2d(info_tup[1])
+        results_dict[f"{term_name}-convperc"] = np.atleast_2d(info_tup[1])
         results_dict[f"{term_name}-jhj"] = jhj
 
     gc.collect()

quartical/gains/general/factories.py
@@ -650,10 +650,11 @@ def iabsdiv_factory(mode):
 
     if mode.literal_value == 4:
         def impl(v1):
-            v1[0] = 0 if v1[0] == 0 else 1/np.abs(v1[0])
-            v1[1] = 0 if v1[1] == 0 else 1/np.abs(v1[1])
-            v1[2] = 0 if v1[2] == 0 else 1/np.abs(v1[2])
-            v1[3] = 0 if v1[3] == 0 else 1/np.abs(v1[3])
+            denom = np.abs(v1[0]) + np.abs(v1[3])
+            if denom != 0:
+                v1[:] = 2/denom
+            else:
+                v1[:] = 0
     elif mode.literal_value == 2:
         def impl(v1):
             v1[0] = 0 if v1[0] == 0 else 1/np.abs(v1[0])