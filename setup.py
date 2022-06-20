9874a2d690dd6d53ee3206601a3419764d1af53f
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-06-20 14:30:00 +0200

    Bump to 0.1.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
 
 setup(
     name='quartical',
-    version='0.1.4',
+    version='0.1.5',
     description="Fast calibration implementation exploiting complex "
                 "optimisation.",
     url='https://github.com/JSKenyon/QuartiCal',
