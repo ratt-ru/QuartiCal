a80cf4d9ce91cb5f93eb5726a6cae214f5e94454
Cyndie Russeeawon <crusseeaw@gmail.com>
2023-03-09 19:49:12 +0200

    First draft of tec estimate

--- quartical/gains/tec/__init__.py
+++ quartical/gains/tec/__init__.py
@@ -1,7 +1,7 @@
 from quartical.gains.gain import Gain, gain_spec_tup, param_spec_tup
 from quartical.gains.tec.kernel import tec_solver, tec_args
 import numpy as np
-
+import finufft
 
 class TEC(Gain):
 
@@ -76,3 +76,121 @@ class TEC(Gain):
         f_map_arr[0, :] = np.arange(n_chan)
 
         return f_map_arr
+
+
+    @staticmethod 
+    def init_term( gain, param, term_ind, term_spec, term_opts, ref_ant, **kwargs ):
+        """Initialise the gains (and parameters)."""
+ 
+        loaded = Gain.init_term(
+            gain, param, term_ind, term_spec, term_opts, ref_ant, **kwargs
+        )
+
+        if loaded or not term_opts.initial_estimate: 
+            return
+
+        data = kwargs["data"] # (row, chan, corr)
+        flags = kwargs["flags"] # (row, chan)
+        a1 = kwargs["a1"]
+        a2 = kwargs["a2"]
+        chan_freq = kwargs["chan_freqs"]
+        t_map = kwargs["t_map_arr"][0, :, term_ind] # time -> solint
+        f_map = kwargs["f_map_arr"][1, :, term_ind] # freq -> solint 
+        _, n_chan, n_ant, n_dir, n_corr = gain.shape
+        
+        # We only need the baselines which include the ref_ant.
+        sel = np.where((a1 == ref_ant) | (a2 == ref_ant))
+        a1 = a1[sel]
+        a2 = a2[sel]
+        t_map = t_map[sel]
+        data = data[sel]
+        flags = flags[sel]
+        data[flags == 1] = 0 # Ignore UV-cut, otherwise there may be no est. 
+        utint = np.unique(t_map)
+        ufint = np.unique(f_map)
+
+        for ut in utint:
+            sel = np.where((t_map == ut) & (a1 != a2))
+            ant_map_pq = np.where(a1[sel] == ref_ant, a2[sel], 0)
+            ant_map_qp = np.where(a2[sel] == ref_ant, a1[sel], 0)
+            ant_map = ant_map_pq + ant_map_qp
+            ref_data = np.zeros((n_ant, n_chan, n_corr), dtype=np.complex128)
+            counts = np.zeros((n_ant, n_chan), dtype=int)
+            np.add.at(
+                ref_data,
+                ant_map,
+                data[sel]
+            )
+            np.add.at(
+                counts,
+                ant_map,
+                flags[sel] == 0 
+            )
+            np.divide(
+                ref_data,
+                counts[:, :, None],
+                where=counts[:, :, None] != 0,
+                out=ref_data
+            )
+
+            for uf in ufint:
+                fsel = np.where(f_map == uf)[0]
+                sel_n_chan = fsel.size
+
+                ###fsel_data = visibilities corresponding to the fsel frequencies
+                fsel_data = ref_data[:, fsel]
+                valid_ant = fsel_data.any(axis=(1, 2))
+                ##in inverse frequency domain
+                invfreq = 1./chan_freq 
+                
+                ##factor for rescaling frequency
+                ffactor = 1e11
+                invfreq *= ffactor
+
+                #delta_freq is the smallest difference between the frequency values
+                delta_freq = invfreq[-2] - invfreq[-1]
+                max_tec = 2 * np.pi/ (delta_freq)
+                super_res = 1000
+                nyq_freq = 1./(2*(invfreq.max()-invfreq.min()))
+                lim0 = 0.5 * max_tec
+
+                ##choosing resolution
+                n = int(super_res * max_tec/ nyq_freq)
+                #domain to pick tec_est from
+                fft_freq = np.linspace(-lim0, lim0, n)
+
+                tec_est = np.zeros((n_ant, n_corr), dtype=np.float64)
+                fft_arr = np.zeros((n_ant, n, n_corr), dtype=fsel_data.dtype)
+
+                for p in range(n_ant): 
+                    for k in range(n_corr):
+                        vis_finufft = finufft.nufft1d3(invfreq, fsel_data[p, :, k], fft_freq, eps=1e-11, isign=-1)
+                        fft_arr[p, :, k] = vis_finufft
+                        fft_data_pk = np.abs(vis_finufft)
+                        tec_est[p, k] = fft_freq[np.argmax(fft_data_pk)]
+
+
+                tec_est[~valid_ant, :] = 0
+
+                for t, p, q in zip(t_map[sel], a1[sel], a2[sel]):
+                    if p == ref_ant:
+                        param[t, uf, q, 0, 1] = -tec_est[q, 0]
+                        if n_corr > 1:
+                            param[t, uf, q, 0, 3] = -tec_est[q, 1]
+                        else:
+                            param[t, uf, p, 0, 1] = tec_est[p, 0]
+                        if n_corr > 1:
+                            param[t, uf, p, 0, 3] = tec_est[p, 1]
+
+                np.save("/home/russeeawon/testing/testing_tecest_quartical/tecest.npy", tec_est)
+                np.save("/home/russeeawon/testing/testing_tecest_quartical/fftarr.npy", fft_arr)
+        
+        import ipdb; ipdb.set_trace()
+        for ut in utint:
+            for f in range(n_chan):
+                fm = f_map[f]
+                cf = 2j * np.pi/ chan_freq[f]
+                gain[ut, f, :, :, 0] = np.exp(cf * param[ut, fm, :, :, 1])
+
+                if n_corr > 1:
+                    gain[ut, f, :, :, -1] = np.exp(cf * param[ut, fm, :, :, 3])
