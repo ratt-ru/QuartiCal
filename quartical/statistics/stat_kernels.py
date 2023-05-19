1ded444274046f9aded7dc9828797c386fb09769
JSKenyon <jonathan.simon.kenyon@gmail.com>
2023-05-19 15:27:44 +0200

    Move stat kernels to overload.

--- quartical/statistics/stat_kernels.py
+++ quartical/statistics/stat_kernels.py
@@ -1,47 +1,116 @@
 import numpy as np
-from numba import prange
-from quartical.utils.numba import coerce_literal
-from quartical.gains.general.generics import qcgjit_parallel
+from numba import njit, prange
+from numba.extending import overload
+from quartical.utils.numba import (coerce_literal,
+                                   JIT_OPTIONS,
+                                   PARALLEL_JIT_OPTIONS)
 from quartical.gains.general.convenience import get_dims, get_row
 import quartical.gains.general.factories as factories
 
 
-@qcgjit_parallel
+@njit(**JIT_OPTIONS)
 def compute_mean_presolve_chisq(
     data,
     model,
     weights,
-    flags
+    flags,
+    row_map,
+    row_weights,
+    corr_mode
+):
+    return compute_mean_presolve_chisq_impl(
+        data,
+        model,
+        weights,
+        flags,
+        row_map,
+        row_weights,
+        corr_mode
+    )
+
+
+def compute_mean_presolve_chisq_impl(
+    data,
+    model,
+    weights,
+    flags,
+    row_map,
+    row_weights,
+    corr_mode
+):
+    return NotImplementedError
+
+
+@overload(compute_mean_presolve_chisq_impl, jit_options=PARALLEL_JIT_OPTIONS)
+def nb_compute_mean_presolve_chisq_impl(
+    data,
+    model,
+    weights,
+    flags,
+    row_map,
+    row_weights,
+    corr_mode
 ):
 
-    n_rows, n_chan, n_dir, n_corr = model.shape
+    coerce_literal(nb_compute_mean_presolve_chisq_impl, ["corr_mode"])
 
-    chisq = 0
-    counts = 0
+    imul_rweight = factories.imul_rweight_factory(corr_mode, row_weights)
+    isub = factories.isub_factory(corr_mode)
+    iunpack = factories.iunpack_factory(corr_mode)
+    valloc = factories.valloc_factory(corr_mode)
 
-    for row in prange(n_rows):
-        tmp = np.empty((n_corr,), dtype=np.complex128)
-        for chan in range(n_chan):
-            tmp[:] = data[row, chan]
-            for dir in range(n_dir):
-                for corr in range(n_corr):
-                    tmp[corr] -= model[row, chan, dir, corr]
-            for corr in range(n_corr):
-                if flags[row, chan] != 1:
-                    w = weights[row, chan, corr]
-                    r = tmp[corr]
-                    chisq += (r.conjugate() * w * r).real
+    def impl(
+        data,
+        model,
+        weights,
+        flags,
+        row_map,
+        row_weights,
+        corr_mode
+    ):
+
+        n_rows, n_chan, n_dir, n_corr = get_dims(model, row_map)
+
+        chisq = 0
+        counts = 0
+
+        for row_ind in prange(n_rows):
+
+            row = get_row(row_ind, row_map)
+            r = valloc(np.complex128)  # Hold data.
+            v = valloc(np.complex128)  # Hold GMGH.
+
+            for f in range(n_chan):
+
+                if flags[row, f]:
+                    continue
+
+                iunpack(r, data[row, f])
+                imul_rweight(r, r, row_weights, row_ind)
+                m = model[row, f]
+                w = weights[row, f]
+
+                for d in range(n_dir):
+
+                    iunpack(v, m[d])
+                    imul_rweight(v, v, row_weights, row_ind)
+                    isub(r, v)
+
+                for c in range(n_corr):
+                    chisq += (r[c].conjugate() * w[c] * r[c]).real
                     counts += 1
 
-    if counts:
-        chisq /= counts
-    else:
-        chisq = np.nan
+        if counts:
+            chisq /= counts
+        else:
+            chisq = np.nan
 
-    return np.array([[chisq]], dtype=np.float64)
+        return np.array([[chisq]], dtype=np.float64)
+
+    return impl
 
 
-@qcgjit_parallel
+@njit(**JIT_OPTIONS)
 def compute_mean_postsolve_chisq(
     data,
     model,
@@ -57,8 +126,59 @@ def compute_mean_postsolve_chisq(
     dir_maps,
     corr_mode
 ):
+    return compute_mean_postsolve_chisq_impl(
+        data,
+        model,
+        weights,
+        flags,
+        a1,
+        a2,
+        row_map,
+        row_weights,
+        gains,
+        time_maps,
+        freq_maps,
+        dir_maps,
+        corr_mode
+    )
 
-    coerce_literal(compute_mean_postsolve_chisq, ["corr_mode"])
+
+def compute_mean_postsolve_chisq_impl(
+    data,
+    model,
+    weights,
+    flags,
+    a1,
+    a2,
+    row_map,
+    row_weights,
+    gains,
+    time_maps,
+    freq_maps,
+    dir_maps,
+    corr_mode
+):
+    return NotImplementedError
+
+
+@overload(compute_mean_postsolve_chisq_impl, jit_options=PARALLEL_JIT_OPTIONS)
+def nb_compute_mean_postsolve_chisq_impl(
+    data,
+    model,
+    weights,
+    flags,
+    a1,
+    a2,
+    row_map,
+    row_weights,
+    gains,
+    time_maps,
+    freq_maps,
+    dir_maps,
+    corr_mode
+):
+
+    coerce_literal(nb_compute_mean_postsolve_chisq_impl, ["corr_mode"])
 
     imul_rweight = factories.imul_rweight_factory(corr_mode, row_weights)
     v1_imul_v2 = factories.v1_imul_v2_factory(corr_mode)
