f8316f272a058daa02deb8988a4f8dad5aefd5c1
Cyndie Russeeawon <crusseeaw@gmail.com>
2023-05-23 12:13:03 +0200

    Fixed compute_jhwj_jhwr for corr 1

--- quartical/gains/delay/kernel.py
+++ quartical/gains/delay/kernel.py
@@ -172,7 +172,7 @@ def delay_solver(base_args, term_args, meta_args, corr_mode):
                              meta_args)
 
         active_params[..., 1::2] /= min_freq  # Undo scaling for SI units.
-
+        np.save("/home/russeeawon/testing/testing_tecest_quartical/jhj_delay.npy", native_imdry.jhj)
         return native_imdry.jhj, term_conv_info(loop_idx + 1, conv_perc)
 
     return impl
