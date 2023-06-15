d3ea45a748a55839bf1972bd10850cc68f91b71c
JSKenyon <jonathan.simon.kenyon@gmail.com>
2023-06-15 14:44:58 +0200

    directory -> path

--- quartical/utils/__init__.py
+++ quartical/utils/__init__.py
@@ -1,7 +1,7 @@
 from daskms.fsspec_store import DaskMSStore
 
 
-def remove_store(directory):
-    store = DaskMSStore(directory)
+def remove_store(path):
+    store = DaskMSStore(path)
     if store.exists():
         store.rm(recursive=True)
