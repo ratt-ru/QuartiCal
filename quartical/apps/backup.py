f8de83e52f23ef06968c4d021d9cc3894e482df1
Landman Bester <lbester@sarao.ac.za>
2023-10-25 11:13:53 +0200

    Don't allow restore app to overwrite metadata (#307)
    * assign to ms to avoid over-writing metadata in restore app
    
    * zip datasets in enumerate
    
    * add comment to document failure case
    
    * use backup_column_name in restore app
    
    * Apply OCD.
    
    ---------
    
    Co-authored-by: landmanbester <lbester@ska.ac.za>
    Co-authored-by: JSKenyon <jonathan.simon.kenyon@gmail.com>

--- quartical/apps/backup.py
+++ quartical/apps/backup.py
@@ -135,9 +135,26 @@ def restore():
     zarr_root, zarr_name = args.zarr_path.url.rsplit("/", 1)
 
     zarr_xds_list = xds_from_zarr(f"{zarr_root}::{zarr_name}")
+    backup_column_name = list(zarr_xds_list[0].data_vars.keys()).pop()
+
+    # This will fail if the column does not exist but if we allow all columns
+    # we need to select out the relevant dims for rechunking below
+    ms_xds_list = xds_from_storage_ms(
+        args.ms_path,
+        columns=(args.column_name,),
+        index_cols=("TIME",),
+        group_cols=("FIELD_ID", "DATA_DESC_ID", "SCAN_NUMBER"),
+    )
+
+    for i, (ds, dsr) in enumerate(zip(ms_xds_list, zarr_xds_list)):
+        dsr = dsr.chunk(ds.chunks)
+        data_array = getattr(dsr, backup_column_name)
+        ms_xds_list[i] = ds.assign(
+            {args.column_name: (data_array.dims, data_array.data)}
+        )
 
     restored_xds_list = xds_to_storage_table(
-        zarr_xds_list,
+        ms_xds_list,
         args.ms_path,
         columns=(args.column_name,),
         rechunk=True
